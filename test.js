a881dc6221228ddaf9ab5e38e7f0ddbc94119199
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2025-01-28 09:32:29 +0100

    a

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ console.log("Hello World!");
 console.log("Első javascript kódom");
 
 function kep_beilleszt() {
-    document.getElementById("kep_helye").innerHTML = "<img src='https://developer.apple.com/wwdc24/images/motion/axiju/endframe-small_2x.jpg'>";
+    document.getElementById("kep_helye").innerHTML = "<img src='https://developer.apple.com/wwdc24/images/motion/axiju/endframe-small_2x.jpg' width='25%'>";
 }function kep_kiilleszt() {
     document.getElementById("kep_helye").innerHTML = "";
 }
