fa601122ec41d74383f20b538f9e9ba6f3f0ab6b
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2025-01-28 09:46:10 +0100

    a

--- console_log_tipusok.js
+++ console_log_tipusok.js
@@ -1,3 +1,11 @@
 console.log("Ez egy normál üzenet.")
 console.warn("Ez egy figyelmeztetés.")
 console.error("Ez egy hibauzenet.")
+
+console.table
+(
+    [
+        {név: "Anna", kor:25},
+        {név: "Béla", kor:30},
+    ]
+)
